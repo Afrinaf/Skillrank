1e8d6ced8b3ef83f8470a5fd7cff2303e069cbca
Afrinaf <107719683+Afrinaf@users.noreply.github.com>
2024-05-05 00:38:27 +0530

    Assessment3

---
+++ Assessment3/backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+
+with open('C:\\python trainig\\Assessment3\\backend\\login.json', 'r') as f:
+    users = json.load(f)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username in users and users[username] == password:
+        return jsonify({'message': 'Login successful'})
+    else:
+        return jsonify({'message': 'Login failed'}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
